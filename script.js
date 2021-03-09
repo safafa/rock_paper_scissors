252fadd863d52b71a33262ba40ba3da7e6bac739
safafa <safaaballagh97@gmail.com>
2021-03-09 18:35:52 +0100

    add playRound function

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ const computer = ["rock","paper","scissors"];
 return computer[Math.floor(Math.random()*computer.length)] ;
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+    const p = playerSelection.toLowerCase();
+    if(p === computerSelection){
+        return "it's a tie"
+    }else{
+        if(p === "rock" && computerSelection === "paper" ){
+            return "You Lose! Paper beats Rock";
+        }
+        if(p === "paper" && computerSelection === "rock" ){
+            return "You Win! Paper beats Rock";
+        }
+        if(p === "scissors" && computerSelection === "paper" ){
+            return "You Win! Scissors  beats Paper";
+        }
+        if(p === "paper" && computerSelection === "scissors" ){
+            return "You Lose! Scissors  beats Paper";
+        }
+      
+        if(p === "rock" && computerSelection === "scissors" ){
+            return "You Win! Rock beats Scissors";
+        }
+        if(p === "scissors" && computerSelection === "rock" ){
+            return "You Loose! Rock beats Scissors";
+        }
+      
+    }
+    
+    
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
